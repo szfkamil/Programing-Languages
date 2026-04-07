536a4fdfbd41dd7d8dd6e34d228f57c3399f18cc
efai6 <destrdima@gmail.com>
2026-04-08 00:43:02 +0200

    queen moves check function added. Pieces spawn fixed

--- Project03/main.py
+++ Project03/main.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import os
+import pygame_widgets
 from pygame.locals import *
 square_size = 120
 
@@ -9,12 +10,27 @@ def pieces_draw():
         for j in range (8):
             if board[i][j] !=0 :
                 if board[i][j] == 9:
-                    screen.blit(queen_img, (300 + square_size * j + 1/5*square_size, 100 + square_size * i + 1/9*square_size))
+                    screen.blit(queen_img, (20 + square_size * j + 1/5*square_size, 20 + square_size * i + 1/9*square_size))
                 elif board[i][j] == 1:
-                    screen.blit(pawn_img, (300 + square_size * j + 1/5*square_size, 100 + square_size * i + 1/9*square_size))
-                    pass
+                    screen.blit(pawn_img, (20 + square_size * j + 1/5*square_size, 20 + square_size * i + 1/9*square_size))
 
     print(board)
+
+def queen_vision(board, coords_list, queens_quantity):
+    directions = [(0,1), (1,0), (1,1), (-1,0), (-1,1), (0,-1), (1,-1), (-1,-1)]
+    for q in range(queens_quantity):
+        for dr, dc in directions:
+            for i in range(1,8):
+                check_x, check_y = coords_list[q][1] + dr * i, coords_list[q][0] + dc * i
+                if 0 <= check_x < 8 and 0 <= check_y < 8:
+                    if board[check_y][check_x] == 1:
+                        print(f"The pawn is attacked by queen on square {coords_list[q][1]}, {coords_list[q][0]}")
+                        print(f"pawn position: {check_x}, {check_y}")
+                        break
+                    elif board[check_y][check_x] != 0:
+                        break
+                else:
+                    break
 
 
 def board_init():
@@ -25,29 +41,32 @@ def board_draw():
     for i in range(8):
         for j in range (8):
             if (i + j) % 2 == 0:
-                pygame.draw.rect(screen, (255, 255, 255), [300 + square_size * j, 100 + square_size * i, 120, 120], 0)
+                pygame.draw.rect(screen, (255, 255, 255), [20 + square_size * j, 20 + square_size * i, 120, 120], 0)
             elif (i + j) % 2 != 0:
-                pygame.draw.rect(screen, (0, 0, 0), [300 + square_size * j, 100 + square_size * i, 120, 120], 0)
+                pygame.draw.rect(screen, (0, 0, 0), [20 + square_size * j, 20 + square_size * i, 120, 120], 0)
 
 def random_pieces_spawn(board):
     os.system('clear')
-    random.randint(0,7)
     k = int(input("How many queens do you want to insert? "))
-    for i in range(k):
-        y = random.randint(0,7)
-        x = random.randint(0,7)
-        if board[y][x] == 0:
-            board[y][x] = 9
     y = random.randint(0,7)
     x = random.randint(0,7)
-    if board[y][x] == 0:
-        board[y][x] = 1
+    coords_list = []
+    board[y][x] = 1
+    for _ in range(k):
+        y = random.randint(0,7)
+        x = random.randint(0,7)
+        while board[y][x] != 0:
+            y = random.randint(0,7)
+            x = random.randint(0,7)
+        board[y][x] = 9
+        coords_list.append((y,x)) 
+    queen_vision(board, coords_list, k)
     return board
 
 pygame.init()
 board = board_init()
 board = random_pieces_spawn(board)
-screen = pygame.display.set_mode((1920, 1080))
+screen = pygame.display.set_mode((1000, 1100))
 pygame.display.set_caption("Window")
 
 try:
